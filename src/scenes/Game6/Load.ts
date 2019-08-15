45522b76dff3a11310f191a375736e047b96b484
彭江 <pengjiang@civaonline.cn>
2019-08-15 10:33:21 +0800

    完成预加载优化

--- src/scenes/Game6/Load.ts
+++ src/scenes/Game6/Load.ts
@@ -4,12 +4,16 @@ import {get , makeParams} from '../../lib/http';
 import {Game6DataItem,game6asset} from '../../interface/Game6';
 
 export default class Game6LoadScene extends Phaser.Scene {
+  private _loader:Phaser.Loader.LoaderPlugin;
   private ccData : Array<Game6DataItem> = [];
   private centerText : Phaser.GameObjects.Text; //文本内容
   private assets:game6asset[] = [{"url":"assets/Game6/analysis.png","key":"analysis"},{"url":"assets/Game6/bg.png","key":"bg"},{"url":"assets/Game6/bgm.mp3","key":"bgm"},{"url":"assets/Game6/btn_exit.png","key":"btn_exit"},{"url":"assets/Game6/btn_last_1.png","key":"btn_last_1"},{"url":"assets/Game6/btn_last_2.png","key":"btn_last_2"},{"url":"assets/Game6/btn_luyin.png","key":"btn_luyin"},{"url":"assets/Game6/btn_luyin_progress.png","key":"btn_luyin_progress"},{"url":"assets/Game6/btn_sound_off.png","key":"btn_sound_off"},{"url":"assets/Game6/btn_sound_on.png","key":"btn_sound_on"},{"url":"assets/Game6/getSugarGourdWordByBookUnitId.json","key":"getSugarGourdWordByBookUnitId"},{"url":"assets/Game6/img_ballgreen.png","key":"img_ballgreen"},{"url":"assets/Game6/img_ballnull.png","key":"img_ballnull"},{"url":"assets/Game6/img_ballpurple.png","key":"img_ballpurple"},{"url":"assets/Game6/img_ballyellow.png","key":"img_ballyellow"},{"url":"assets/Game6/img_cloud.png","key":"img_cloud"},{"url":"assets/Game6/tips_arrow_left.png","key":"tips_arrow_left"},{"url":"assets/Game6/tips_arrow_right.png","key":"tips_arrow_right"},{"url":"assets/Game6/tips_arrow_up.png","key":"tips_arrow_up"},{"url":"assets/Game6/tips_goodjob.png","key":"tips_goodjob"},{"url":"assets/Game6/tips_no.png","key":"tips_no"},{"url":"assets/Game6/tips_tryagain.png","key":"tips_tryagain"},{"url":"assets/Game6/cover.png","key":"cover"},{"url":"assets/Game6/particles.png","key":"particles"}];
 
+  private bg:Phaser.GameObjects.Image;
+  private cover:Phaser.GameObjects.Image;
+
   private imgLoadDone : boolean = false;  //图片是否加载完毕
-  private dataLoadDone : boolean = false;   //数据是否加载完毕
+  private audioLoadDone : boolean = false;   //数据是否加载完毕
 
   constructor() {
     super({
@@ -25,6 +29,7 @@ export default class Game6LoadScene extends Phaser.Scene {
       font: 'bold 60px Arial',
       bold : true,
     }).setOrigin(.5,.5);
+    this._loader = new Phaser.Loader.LoaderPlugin(this);
   }
 
   preload(): void {
@@ -36,7 +41,7 @@ export default class Game6LoadScene extends Phaser.Scene {
       this.load.image(v.key,v.url);
     })
     this.load.on("progress",(e:any)=>{
-        e = Math.floor(e*100);
+        e = Math.floor(e*50);
         this.centerText.setText(`${e}%`);
     })
     this.load.on("complete",(e:any)=>{
@@ -46,11 +51,11 @@ export default class Game6LoadScene extends Phaser.Scene {
     }
 
   create(): void {
-     let bg = this.add.image(0,0,"bg").setOrigin(0).setAlpha(0.4);
-     let cover = this.add.image(1024*0.5,552*0.5,"cover");
-     cover.setInteractive();
-     cover.on("pointerdown",()=>{
-      if(!this.dataLoadDone || !this.imgLoadDone){
+     this.bg = this.add.image(0,0,"bg").setOrigin(0).setAlpha(0);
+     this.cover = this.add.image(1024*0.5,552*0.5,"cover").setAlpha(0);
+     this.cover.setInteractive();
+     this.cover.on("pointerdown",()=>{
+      if(!this.audioLoadDone || !this.imgLoadDone){
         return false;
       }
       this.scene.start('Game6PlayScene',{
@@ -74,17 +79,37 @@ export default class Game6LoadScene extends Phaser.Scene {
       `)
   }
 
+  /**
+   * 正式状态
+   */
   private getData(){
-    /**
-     * 正式状态
-     */
-    //get(apiPath.getWordsData).then((res)=>{
     get("assets/Game6/getSugarGourdWordByBookUnitId.json").then((res)=>{
       res&&res.code==='0000'&&(this.ccData = res.result);
     }).then(()=>{
-      this.dataLoadDone = true;
-      
+      this.audioLoadDone = true;
+      this.loadAudio();
     })
+  }
+
+  /**
+   * 加载音频
+   */
+  private loadAudio():void{
+      this.ccData.forEach(v=>{
+        this._loader.audio(v.name,v.audioKey);
+        v.phoneticSymbols.forEach(_v=>{
+          this._loader.audio(_v.name,_v.audioKey);
+        })
+      })
+    this._loader.on("progress",(e:any)=>{
+        e = Math.floor(50 + e*50);
+        this.centerText.setText(`${e}%`);
+    })
+      this._loader.on("complete",()=>{
+        this.bg.setAlpha(0.4);
+        this.cover.setAlpha(1);
+      });
+      this._loader.start();
   }
 
 };
