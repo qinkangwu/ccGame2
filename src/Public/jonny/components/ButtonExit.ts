0d0ba9b3fc01359f43967f8f5cef7a230952229b
彭江 <pengjiang@civaonline.cn>
2019-08-19 14:51:13 +0800

    sync

---
+++ src/Public/jonny/components/ButtonExit.ts
@@ -0,0 +1,23 @@
+import 'phaser';
+import Button from './Button';
+
+/**
+  * @param Phaser.Scene [scene]  
+  * @param number [x] options
+  * @param number [y] options
+  * @param string [texture] options
+  * @param any [shape] options
+  * @param Function [callback] options 
+ */
+
+export default class ButtonExit extends Button {
+    mountUpdate: Function;
+    constructor(scene: Phaser.Scene, x: number = 25 + 60 * 0.5, y:number = 25 + 60 * 0.5,texture: string = "btn_exit", shape: any = new Phaser.Geom.Circle(60 * 0.5, 60 * 0.5, 60), callback: Phaser.Types.Input.HitAreaCallback = Phaser.Geom.Circle.Contains) {
+      super(scene, x, y, texture, shape, callback)
+      this.pointerdownFunc = this.exitGame;
+    }
+  
+    exitGame(): void {
+        alert("Game Exit");
+    }
+  }
