9ac209d2ada2917d2ddafafc23e16d83327c7fa3
彭江 <pengjiang@civaonline.cn>
2019-08-22 11:48:48 +0800

    整合button的UI

--- src/Public/jonny/StaticAni.ts
+++ src/Public/jonny/StaticAni.ts
@@ -1,7 +1,23 @@
+import 'phaser';
+
 export class StaticAni{
     public alphaScaleFuc(obj, _scaleX: number, _scaleY: number, _alpha: number):void {
         obj.scaleX = _scaleX;
         obj.scaleY = _scaleY;
         obj.alpha = _alpha;
+    }
+}
+
+export class TweenAni{
+    public alphaScaleYoyoFunc(scene:Phaser.Scene,obj:Phaser.GameObjects.Image,scaleX:number,scaleY:number,alpha:number){
+        scene.tweens.add({
+            targets : obj,
+            scaleX : scaleX,
+            scaleY : scaleY,
+            alpha:alpha,
+            duration : 200,
+            ease : 'Sine.easeInOut',
+            yoyo:true
+         })
     }
 }
