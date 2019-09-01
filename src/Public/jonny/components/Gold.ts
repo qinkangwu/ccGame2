5c68eb86f70a24bfe94c35a6b4eae9844ec5dbf2
彭江 <jonny.peng@qq.com>
2019-09-01 17:58:45 +0800

    修改字号

--- src/Public/jonny/components/Gold.ts
+++ src/Public/jonny/components/Gold.ts
@@ -13,7 +13,7 @@ export class Gold extends Phaser.GameObjects.Container{
         super(scene);
         value=value.toString();
         this.goldImg = new Phaser.GameObjects.Image(scene,Gold.imgPosition.x,Gold.imgPosition.y, "goldValue");
-        this.goldText = new Phaser.GameObjects.Text(scene,981.45, 167.45,value, <Phaser.Types.GameObjects.Text.TextSyle>{ align: "center", fontSize: "12px", fontFamily: "Arial", stroke: "#fff", strokeThickness: 1 }).setOrigin(0.5);
+        this.goldText = new Phaser.GameObjects.Text(scene,981.45, 167.45,value, <Phaser.Types.GameObjects.Text.TextSyle>{ align: "center", fontSize: "16px", fontFamily: "Arial", stroke: "#fff", strokeThickness: 1 }).setOrigin(0.5);
         this.add([this.goldImg,this.goldText]);
     }
 
