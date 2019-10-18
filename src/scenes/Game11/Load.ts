9f5986345f2dffa059c02744b6e8b0a861c4258c
彭江 <jonny.peng@qq.com>
2019-10-18 14:13:37 +0800

    过滤长度大于9的数据

--- src/scenes/Game11/Load.ts
+++ src/scenes/Game11/Load.ts
@@ -1,9 +1,9 @@
 import 'phaser';
 import apiPath from '../../lib/apiPath';
 import { get } from '../../lib/http';
-import { Assets,Game11DataItem,GetSentenceData} from '../../interface/Game11';
+import { Assets, Game11DataItem, GetSentenceData } from '../../interface/Game11';
 import { resize } from '../../Public/jonny/core';
-import { SellingGold ,TryAginListenBtn} from '../../Public/jonny/components';
+import { SellingGold, TryAginListenBtn } from '../../Public/jonny/components';
 import PlanAnims from '../../Public/PlanAnims';
 import TipsParticlesEmitter from '../../Public/TipsParticlesEmitter';
 
@@ -14,7 +14,7 @@ export default class Game11LoadScene extends Phaser.Scene {
   private _loader: Phaser.Loader.LoaderPlugin;
   private ccData: Array<Game11DataItem> = [];
   private centerText: Phaser.GameObjects.Text; //文本内容
-  private assets:Assets[] = [{"url":"assets/mask/Game11.png","key":"Game11"},{"url":"assets/commonUI/successBtn.png","key":"successBtn"},{"url":"assets/commonUI/btnSoundOff.png","key":"btnSoundOff"},{"url":"assets/commonUI/btnSoundOn.png","key":"btnSoundOn"},{"url":"assets/commonUI/btnExit.png","key":"btnExit"},{"url":"assets/commonUI/originalSoundBtn.png","key":"originalSoundBtn"},{"url":"assets/commonUI/listenAgain.png","key":"listenAgain"},{"url":"assets/Game11/locomotive.png","key":"locomotive"},{"url":"assets/Game11/rope.png","key":"rope"},{"url":"assets/Game11/symbolTrainBox.png","key":"symbolTrainBox"},{"url":"assets/Game11/trainBox.png","key":"trainBox"},{"url":"assets/commonUI/goldValue.png","key":"goldValue"},{"url":"assets/Game11/bg.png","key":"bg"},{"url":"assets/Game11/ground.png","key":"ground"},{"url":"assets/Game11/bgFull.png","key":"bgFull"},{"url":"assets/commonUI/tipsArrowUp.png","key":"tipsArrowUp"}];
+  private assets: Assets[] = [{ "url": "assets/mask/Game11.png", "key": "Game11" }, { "url": "assets/commonUI/successBtn.png", "key": "successBtn" }, { "url": "assets/commonUI/btnSoundOff.png", "key": "btnSoundOff" }, { "url": "assets/commonUI/btnSoundOn.png", "key": "btnSoundOn" }, { "url": "assets/commonUI/btnExit.png", "key": "btnExit" }, { "url": "assets/commonUI/originalSoundBtn.png", "key": "originalSoundBtn" }, { "url": "assets/commonUI/listenAgain.png", "key": "listenAgain" }, { "url": "assets/Game11/locomotive.png", "key": "locomotive" }, { "url": "assets/Game11/rope.png", "key": "rope" }, { "url": "assets/Game11/symbolTrainBox.png", "key": "symbolTrainBox" }, { "url": "assets/Game11/trainBox.png", "key": "trainBox" }, { "url": "assets/commonUI/goldValue.png", "key": "goldValue" }, { "url": "assets/Game11/bg.png", "key": "bg" }, { "url": "assets/Game11/ground.png", "key": "ground" }, { "url": "assets/Game11/bgFull.png", "key": "bgFull" }, { "url": "assets/commonUI/tipsArrowUp.png", "key": "tipsArrowUp" }];
   constructor() {
     super({
       key: "Game11LoadScene"
@@ -33,7 +33,7 @@ export default class Game11LoadScene extends Phaser.Scene {
   }
 
   preload(): void {
-    this.load.json("trainboxShape","assets/Game11/trainboxShape.json");
+    this.load.json("trainboxShape", "assets/Game11/trainboxShape.json");
     this.load.audio('bgm', 'assets/sounds/bgm-03.mp3');
     this.load.bitmapFont('ArialRoundedBold30', 'assets/font/ArialRoundedBold30/font.png', 'assets/font/ArialRoundedBold30/font.xml');
     TipsParticlesEmitter.loadImg(this);
@@ -60,22 +60,26 @@ export default class Game11LoadScene extends Phaser.Scene {
    */
   private getData() {
     get(apiPath.getSentenceData).then((res) => {
-      if(res.code==='0000'){
-       this.ccData = (<GetSentenceData[]>res.result).map(v=>{
-        delete v.id; 
-        delete v.videoId;
-        delete v.imgKey;
-        v.name = v.name.replace(/\s/g,"");
-        v.vocabularies.map(_v=>{
-          delete _v.id;
-          delete _v.videoId;
-          delete _v.img;
-          delete _v.syllable;
-          delete _v.phoneticSymbol;
-          return _v;
-        });
-        return v;
-      })
+      if (res.code === '0000') {
+        this.ccData = (<GetSentenceData[]>res.result)
+          .map(v => {
+            delete v.id;
+            delete v.videoId;
+            delete v.imgKey;
+            v.name = v.name.replace(/\s/g, "");
+            v.vocabularies.map(_v => {
+              delete _v.id;
+              delete _v.videoId;
+              delete _v.img;
+              delete _v.syllable;
+              delete _v.phoneticSymbol;
+              return _v;
+            });
+            return v;
+          })
+          .filter(v=>{
+            return v.vocabularies.length < 9;   //过滤长度大于9的数据
+          })
       }
     }).then(() => {
       this.loadAudio();
