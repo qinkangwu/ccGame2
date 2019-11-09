f5637a95a0f629ac497a9b5206858258a600020c
彭江 <jonny.peng@qq.com>
2019-11-09 17:12:45 +0800

    准备重构提示组件

--- src/scenes/Game16/Play.ts
+++ src/scenes/Game16/Play.ts
@@ -1,20 +1,22 @@
 import 'phaser';
-import { Subject} from 'rxjs';
+import { Subject } from 'rxjs';
 import { Assets, Topic } from '../../interface/Game16';
 import { cover, rotateTips, isHit, Vec2, CONSTANT } from '../../Public/jonny/core';
 import { Button, ButtonMusic, ButtonExit, SellingGold, Gold } from '../../Public/jonny/components';
 import TipsParticlesEmitter from '../../Public/TipsParticlesEmitter';
-import { Door,IndexText,OrderUI,Blood} from '../../Public/jonny/game16';
+import { Door, IndexText, OrderUI, Blood } from '../../Public/jonny/game16';
 
 const vol = 0.3; //背景音乐的音量
 const W = 1024;
 const H = 552;
 var index: number; //题目的指针，默认为0
-var goldValue: number = 3; //金币的值
+
+var goldValue: number = 6; //金币的值
 
 export interface Determine {
     isRight: boolean;
-    times: number;
+    angelBlood: number;
+    devilBlood: number;
 }
 
 
@@ -22,27 +24,28 @@ export interface Determine {
 
 export default class Game16PlayScene extends Phaser.Scene {
     private ccData: Array<Topic> = [];
-    private times: number = 0;  //次数
-
+    //private times: number = 0;  //次数
+    private blood2Index: number = 0;
+    private blood8Index: number = 0;
     //静态开始
     private bgm: Phaser.Sound.BaseSound; //背景音乐
     private btnExit: Button;  //退出按钮
     private btnSound: ButtonMusic; //音乐按钮
     private gold: Gold;
-    private blood:Blood;
+    private blood: Blood;
     //静态结束
 
     // 动态开始
-    private orderUI:OrderUI;
+    private orderUI: OrderUI;
     private door: Door;
-    private indexText:IndexText;
+    private indexText: IndexText;
     private tipsParticlesEmitter: TipsParticlesEmitter;
     private sellingGold: SellingGold;
-    private angelAction:Phaser.GameObjects.Sprite;
-    private devilAction:Phaser.GameObjects.Sprite;
+    private angelAction: Phaser.GameObjects.Sprite;
+    private devilAction: Phaser.GameObjects.Sprite;
 
     //数据
-    private result$:Subject<Determine> = new Subject (); //结果订阅
+    private result$: Subject<Determine> = new Subject(); //结果订阅
 
 
     private layer0: Phaser.GameObjects.Container;
@@ -154,26 +157,26 @@ export default class Game16PlayScene extends Phaser.Scene {
      */
     createActors() {
         // create orderUI,blood
-        this.orderUI = new OrderUI(this,this.ccData[index].trueWord,this.ccData[index].displayWord);
-        this.blood = new Blood(this);
+        this.orderUI = new OrderUI(this, this.ccData[index].trueWord, this.ccData[index].displayWord);
+        this.blood = new Blood(this, this.blood2Index, this.blood8Index);
         this.blood.visible = false;
-        this.layer1.add([this.orderUI,this.blood]);
+        this.layer1.add([this.orderUI, this.blood]);
 
 
         // create angel's action
-        this.angelAction = this.add.sprite(0,0,"angelAction").setDepth(5).setVisible(false);
+        this.angelAction = this.add.sprite(0, 0, "angelAction").setDepth(5).setVisible(false);
         this.anims.create({
-            key:"angelKill",
-            frames:this.anims.generateFrameNames('angelAction',{ prefix:'angelAction1000',start:0,end:12}),
-            frameRate:12
+            key: "angelKill",
+            frames: this.anims.generateFrameNames('angelAction', { prefix: 'angelAction1000', start: 0, end: 12 }),
+            frameRate: 12
         });
 
         // create devil's action
-        this.devilAction = this.add.sprite(0,0,"devilAction").setDepth(5).setVisible(false);
+        this.devilAction = this.add.sprite(0, 0, "devilAction").setDepth(5).setVisible(false);
         this.anims.create({
-            key:"devilKill",
-            frames:this.anims.generateFrameNames('devilAction',{ prefix:'devilAction1000',start:0,end:9}),
-            frameRate:12
+            key: "devilKill",
+            frames: this.anims.generateFrameNames('devilAction', { prefix: 'devilAction1000', start: 0, end: 9 }),
+            frameRate: 12
         });
 
         // create door
@@ -192,13 +195,21 @@ export default class Game16PlayScene extends Phaser.Scene {
      * 观察者
      */
     private observableFuc() {
-        this.result$.subscribe(value=>{
-            if(value.isRight===true){
-                //this.angelActing();
-                this.isRight();
-            }else if(value.isRight===false){
-                this.isWrong();
-                //this.devilActing();
+        this.result$.subscribe(value => {
+            console.log(value);
+            if (value.isRight === true) {
+                this.blood8Index += 1;
+                this.blood.setBlood8(this.blood8Index);
+                this.angelActing().then(this.nextRound.bind(this))
+                
+            } else if (value.isRight === false && value.angelBlood === 0) {
+                this.blood2Index += 1;
+                this.blood.setBlood2(this.blood2Index);
+                this.devilActing().then(this.tryAgin.bind(this));
+            } else if (value.isRight === false && value.angelBlood === 1) {
+                this.blood2Index += 1;
+                this.blood.setBlood2(this.blood2Index);
+                this.devilActing().then(this.ohNo.bind(this));
             }
         });
     }
@@ -206,47 +217,42 @@ export default class Game16PlayScene extends Phaser.Scene {
     /**
      * 游戏开始
      */
-    private async gameStart(){
-        let _index:string = index + 1  < 10 ? "0" + (index+1) : (index + 1).toString();
+    private async gameStart() {
+        let _index: string = index + 1 < 10 ? "0" + (index + 1) : (index + 1).toString();
         await this.door.close();
         await this.indexText.show(_index);
         this.orderUI.show();
         this.indexText.hide();
         this.blood.visible = true;
         await this.door.open();
-        this.observableFuc();
-
-        // this.T$ = fromEvent(this.orderUI.trueBtn,"pointerdown");
-        // this.F$ = fromEvent(this.orderUI.falseBtn,"pointerdown");
-
-
-
-        //this.orderUI.falseBtn.on("pointerdown",);
+        if (index === 0) {
+            this.observableFuc();
+        }
         this.controls();
     }
 
     /**
      * 交互事件
      */
-    private controls():void{
-        let TorF = (decided:number)=>{
-            this.orderUI.trueBtn.disableInteractive(); 
-            this.orderUI.falseBtn.disableInteractive(); 
-            let _isRight:boolean;
-            if(decided===1){
-               _isRight = this.ccData[index].displayWord===this.ccData[index].trueWord;
-            }else{
-               _isRight = this.ccData[index].displayWord!==this.ccData[index].trueWord;
+    private controls(): void {
+        let TorF = (decided: number) => {
+            console.log("执行点击事件");
+            this.disableBtnInteractive();
+            let _isRight: boolean;
+            if (decided === 1) {
+                _isRight = this.ccData[index].displayWord === this.ccData[index].trueWord;
+            } else {
+                _isRight = this.ccData[index].displayWord !== this.ccData[index].trueWord;
             }
             this.result$.next({
-                isRight:_isRight,
-                times:this.times
+                isRight: _isRight,
+                angelBlood: this.blood2Index,
+                devilBlood: this.blood8Index
             });
         }
 
-
-        this.orderUI.trueBtn.on("pointerdown",TorF.bind(this,1));
-        this.orderUI.falseBtn.on("pointerdown",TorF.bind(this,0));
+        this.orderUI.trueBtn.on("pointerdown", TorF.bind(this, 1));
+        this.orderUI.falseBtn.on("pointerdown", TorF.bind(this, 0));
     }
 
     /**
@@ -266,30 +272,31 @@ export default class Game16PlayScene extends Phaser.Scene {
     /**
      * 天使攻击
      */
-    public angelActing():Promise<boolean>{
+    public angelActing(): Promise<boolean> {
         this.audioPlay("heavyBoxing");
-        return this.acting(this.angelAction,this.orderUI.devil.getWorldTransformMatrix().tx,this.orderUI.devil.getWorldTransformMatrix().ty,"angelKill");
+        return this.acting(this.angelAction, this.orderUI.devil.getWorldTransformMatrix().tx, this.orderUI.devil.getWorldTransformMatrix().ty, "angelKill");
     }
 
     /**
      * 恶魔攻击
      */
-    public devilActing():Promise<boolean>{
+    public devilActing(): Promise<boolean> {
         this.audioPlay("defense");
-        return this.acting(this.devilAction,this.orderUI.angel.getWorldTransformMatrix().tx,this.orderUI.angel.getWorldTransformMatrix().ty,"devilKill");
+        return this.acting(this.devilAction, this.orderUI.angel.getWorldTransformMatrix().tx, this.orderUI.angel.getWorldTransformMatrix().ty, "devilKill");
     }
 
     /**
      * 攻击
      */
-    private acting(obj:Phaser.GameObjects.Sprite,x:number,y:number,key:string):Promise<boolean>{
-        return new Promise<boolean>(resolve=>{
+    private acting(obj: Phaser.GameObjects.Sprite, x: number, y: number, key: string): Promise<boolean> {
+        return new Promise<boolean>(resolve => {
             obj.visible = true;
-            obj.setPosition(x,y);
+            obj.setPosition(x, y);
             obj.play(key);
-            setTimeout(()=>{
+            setTimeout(() => {
+                obj.visible = false;
                 resolve(true);
-            },1300);
+            }, 1300);
         });
     }
 
@@ -314,71 +321,104 @@ export default class Game16PlayScene extends Phaser.Scene {
      * 正确的结果处理
      */
     private isRight(): void {
-        let nextFuc = () => {
-            this.sellingGold = new SellingGold(this, {
-                callback: () => {
-                    this.sellingGold.golds.destroy();
-                    this.setGoldValue(3);
-                    this.nextRound();
-                }
-            });
-            this.sellingGold.golds.setDepth(6);
-            this.tipsParticlesEmitter.success(() => {
-                this.sellingGold.goodJob(3);
-            })
-        }
+        // let nextFuc = () => {
+        //     this.sellingGold = new SellingGold(this, {
+        //         callback: () => {
+        //             this.sellingGold.golds.destroy();
+        //             this.setGoldValue(3);
+        //             this.nextRound();
+        //         }
+        //     });
+        //     this.sellingGold.golds.setDepth(6);
+        //     this.tipsParticlesEmitter.success(() => {
+        //         this.sellingGold.goodJob(3);
+        //     })
+        // }
+        // nextFuc();
 
-        this.angelActing().then(nextFuc);
     }
 
     /**
      * 错误的结果处理
      */
     private isWrong(): void {
-        let nextFuc = ()=>{
-            this.times += 1;
-            if (this.times === 1) {
-                this.tryAgin();
-            } else if (this.times >= 2) {
-                this.ohNo();
-            }
-        }
-       
-        this.devilActing().then(nextFuc);
-       
+        // let nextFuc = ()=>{
+        //     this.blood2Index += 1;
+        //     if (this.times === 1) {
+        //         this.tryAgin();
+        //     } else if (this.times >= 2) {
+        //         this.ohNo();
+        //     }
+        // }
+
+        // this.devilActing().then(nextFuc);
+
     }
 
     /**
      * 再玩一次
      */
     private tryAgin() {
-        this.tipsParticlesEmitter.tryAgain(this.resetStart);
-        this.orderUI.trueBtn.setInteractive(); 
-        this.orderUI.falseBtn.setInteractive(); 
+        this.tipsParticlesEmitter.tryAgain(this.enableBtnInteractive);
+    }
+
+
+    private enableBtnInteractive() {
+        this.orderUI.trueBtn.setInteractive();
+        this.orderUI.falseBtn.setInteractive();
+    }
+
+    private disableBtnInteractive() {
+        this.orderUI.trueBtn.disableInteractive();
+        this.orderUI.falseBtn.disableInteractive();
     }
 
     /**
      * 重置开始状态
      */
     private resetStart() {
-
+        this.enableBtnInteractive();
     }
+
 
     /**
      * 再次错误
      */
     private ohNo() {
-        this.setGoldValue(-1);
-        this.tipsParticlesEmitter.error(
-            this.nextRound,
-            this.resetStart
-        )
-        if (goldValue === 0) {
-            setTimeout(() => {
-                this.scene.pause();
-                alert("啊哦，你又错啦！金币不足，一起去赚金币吧");
-            }, 1300)
+        let relive = ()=>{   //复活
+            this.setGoldValue(-2);
+            this.blood2Index = 0;
+            this.blood.setBlood2(this.blood2Index);
         }
+        
+
+        let changeSingle = ()=>{   //换一个
+            relive(); 
+            this.nextRound(); 
+        }
+
+        let replay = ()=>{    //再玩一次
+            relive();
+            this.resetStart();
+        }
+
+        this.tipsParticlesEmitter.error(
+            changeSingle,
+            replay    
+        )
+        // if (goldValue === 0) {
+        //     setTimeout(() => {
+        //         this.scene.pause();
+        //         alert("啊哦，你又错啦！金币不足，一起去赚金币吧");
+        //     }, 1300)
+        // }
+    }
+
+    /**
+     * 销毁组件
+     */
+    private destroyComponent(): void {
+
     }
 
     /**
@@ -386,23 +426,13 @@ export default class Game16PlayScene extends Phaser.Scene {
      */
     private nextRound(): void {
         index += 1;
-        if (index > this.ccData.length - 1) {
-            window.location.href = CONSTANT.INDEX_URL;
-        }
-        this.times = 0;
+        // if (index > this.ccData.length - 1) {
+        //     window.location.href = CONSTANT.INDEX_URL;
+        // }
+        //this.resetStart();
         this.scene.start('Game16PlayScene', {
             data: this.ccData,
             index: index
-        });
-    }
-
-    /**
-     * 判断做题结果是否正确
-     */
-    private checkoutResult(): Promise<string> {
-        let answer: string = "";
-        return new Promise((resolve, reject) => {
-
         });
     }
 
