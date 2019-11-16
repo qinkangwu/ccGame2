39502df0717d83d5b1ffccf25e93e954d2f19422
彭江 <jonny.peng@qq.com>
2019-11-16 18:02:58 +0800

    ready make game19

--- src/Public/jonny/selectTopic/CivaWorker.ts
+++ src/Public/jonny/selectTopic/CivaWorker.ts
@@ -1,54 +1,79 @@
 import "phaser";
 
-export class CivaWorker extends Phaser.GameObjects.Sprite{
-    private time:number = 0;
-    public asBeeDanceAnimate:Phaser.Tweens.Tween;
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string,frame?: string){
-        super(scene,x,y,texture,frame);
+export class CivaWorker extends Phaser.GameObjects.Sprite {
+    private time: number = 0;
+    public asBeeDanceAnimate: Phaser.Tweens.Tween;
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string) {
+        super(scene, x, y, texture, frame);
     }
 
-    public asBee():CivaWorker{
+    /**
+     * 作为一名魔法师
+     */
+    public asWizard(): CivaWorker {
         this.scene.anims.create({
-         key:"flight",
-         frames:this.scene.anims.generateFrameNames("civaBee",{prefix:"civaBee000",start:0,end:9}),
-         repeat:-1,
-         frameRate:60
-       });
-       this.play("flight");
-       return this;
+            key: "fly",
+            frames: this.scene.anims.generateFrameNames("wizard", { prefix: "CivaWizard00", start: 0, end: 19 }),
+            repeat: -1,
+            frameRate:25
+        });
+        this.play("fly");
+        
+        return this;
     }
 
-    public asBeeDance(){
+
+    /**
+     * 作为一名蜜蜂
+     */
+    public asBee(): CivaWorker {
+        this.scene.anims.create({
+            key: "flight",
+            frames: this.scene.anims.generateFrameNames("civaBee", { prefix: "civaBee000", start: 0, end: 9 }),
+            repeat: -1,
+            frameRate: 60
+        });
+        this.play("flight");
+        return this;
+    }
+
+    /**
+     * 当蜜蜂进行跳舞
+     */
+    public asBeeDance() {
         this.asBeeDanceAnimate = this.scene.add.tween({
-            targets:this,
-            y:"+=10",
-            repeat:-1,
-            duration:500,
-            yoyo:true,
-            onUpdate:()=>{
-                this.time+=0.2;
+            targets: this,
+            y: "+=10",
+            repeat: -1,
+            duration: 500,
+            yoyo: true,
+            onUpdate: () => {
+                this.time += 0.2;
                 this.x += Math.sin(this.time);
             }
         });
     }
 
-    public asBeeWorking(x:number,y:number):Promise<boolean>{
+    /**
+     * 当蜜蜂进行工作
+     */
+    public asBeeWorking(x: number, y: number): Promise<boolean> {
         this.asBeeDanceAnimate.stop();
-        return new Promise<boolean>(resolve=>{
+        return new Promise<boolean>(resolve => {
             this.scene.tweens.timeline({
-                targets:this,
-                tweens:[
+                targets: this,
+                tweens: [
                     {
-                        x:x,
-                        y:y,
-                        duration:1500,
-                        onUpdate:()=>{
-                            this.time+=0.1;
-                            this.x += Math.sin(this.time)*10; 
+                        x: x,
+                        y: y,
+                        duration: 1500,
+                        onUpdate: () => {
+                            this.time += 0.1;
+                            this.x += Math.sin(this.time) * 10;
                         }
                     }
                 ],
-                onComplete:()=>{
+                onComplete: () => {
                     this.anims.stop();
                     resolve(true);
                 }
---
+++ src/scenes/Game19/Load.ts
@@ -0,0 +1,139 @@
+import 'phaser';
+import apiPath from '../../lib/apiPath';
+import { get } from '../../lib/http';
+import { Assets, QueryTopic } from '../../interface/SelectTopic';
+import { resize, Vec2 } from '../../Public/jonny/core';
+import { SellingGold, TryAginListenBtn } from '../../Public/jonny/components';
+import TipsParticlesEmitter from '../../Public/TipsParticlesEmitter';
+
+const W = 1024;
+const H = 552;
+
+export default class Game19LoadScene extends Phaser.Scene {
+  private _loader: Phaser.Loader.LoaderPlugin;
+  private ccData: Array<QueryTopic> = [];
+  private centerText: Phaser.GameObjects.Text; //文本内容
+  private assets: Assets[] = [
+    /**
+     * common UI
+     */
+    { "url": "assets/mask/Game19.png", "key": "Game19" }, { "url": "assets/commonUI/successBtn.png", "key": "successBtn" }, { "url": "assets/commonUI/btnSoundOff.png", "key": "btnSoundOff" }, { "url": "assets/commonUI/btnSoundOn.png", "key": "btnSoundOn" }, { "url": "assets/commonUI/btnExit.png", "key": "btnExit" }, { "url": "assets/commonUI/goldValue.png", "key": "goldValue" }, { "url": "assets/commonUI/tipsArrowUp.png", "key": "tipsArrowUp" }
+    /**
+     * game19 UI
+     */
+    , { "url": "assets/Game19/bg.png", "key": "bg" }, { "url": "assets/Game19/daan03.png", "key": "daan" }, { "url": "assets/Game19/tigan02.png", "key": "tigan01" }
+  ];
+
+  constructor() {
+    super({
+      key: "Game19LoadScene"
+    });
+    this.ccData = [];
+  }
+
+  init(): void {
+    resize.call(this, W, H);
+    this.centerText = this.add.text(1024 * 0.5, 552 * 0.5, '0%', {
+      fill: '#fff',
+      font: 'bold 60px Arial',
+      bold: true,
+    }).setOrigin(.5, .5);
+    this._loader = new Phaser.Loader.LoaderPlugin(this);
+  }
+
+  preload(): void {
+    this.load.audio('bgm', 'assets/sounds/bgm-04.mp3');
+    this.load.audio('successMp3', 'assets/sounds/successMp3.mp3');
+    this.load.audio('clickMp3', 'assets/sounds/clickMp3.mp3');
+    this.load.audio('right', 'assets/sounds/newJoin/right.mp3');
+    this.load.audio('wrong', 'assets/sounds/newJoin/wrong.mp3');
+    this.load.atlas("wizard","assets/Game19/wizard.png","assets/Game19/wizard.json");
+    this.load.bitmapFont('ArialRoundedBold', 'assets/font/ArialRoundedBold/font.png', 'assets/font/ArialRoundedBold/font.xml');
+    TipsParticlesEmitter.loadImg(this);
+    TryAginListenBtn.loadAssets(this);
+    SellingGold.loadImg(this);
+    this.assets.forEach((v) => {
+      this.load.image(v.key, v.url);
+    })
+    this.load.on("progress", (e: any) => {
+      e = Math.floor(e * 100);
+      this.centerText.setText(`${e}%`);
+    })
+    this.load.on("complete", (e: any) => {
+      this.getData();
+    })
+  }
+
+  create(): void {
+  }
+
+  /**
+   * 正式状态
+   */
+  private getData() {
+    let serial: string[] = ["A", "B", "C", "D"]
+    let position4: Array<Vec2> = [
+      new Vec2(27 + 256 * 0.5, 322 + 204 * 0.5),
+      new Vec2(284 + 256 * 0.5, 322 + 204 * 0.5),
+      new Vec2(511.5 + 256 * 0.5, 322 + 204 * 0.5),
+      new Vec2(767.5 + 256 * 0.5, 322 + 204 * 0.5)
+    ];
+    let position3: Array<Vec2> = [
+      new Vec2(256-15, 322 + 204 * 0.5),
+      new Vec2(512, 322 + 204 * 0.5),
+      new Vec2(768+15, 322 + 204 * 0.5)
+    ];
+    get(apiPath.getQuestionData).then((res) => {
+      if (res.code === '0000') {
+        this.ccData = (<any>res.result)
+          .filter((v,i)=>i>=40&&i<60)
+          .map(v => {
+            delete v.audiokey;
+            delete v.imgKey;
+            v.questionContent = v.questionContent.replace(/\d+\./,"").replace(/[\?\？]\s*/, "?\n").replace(/^[-—]+/, "").replace(/\s{2,}/, " ").replace(/\?_+/, "?\n").replace(/\n$/, "").replace(/^\s/, "").replace(/\?\s*[—-]+/, "?\n").replace(/\n\s+/, "\n").replace(/&nbsp;/,"").replace(/_+/,"___").replace(/’/,"'");
+            v.answers.forEach((_v, _i) => {
+              delete _v.audioKey;
+              delete _v.imgKey;
+              _v.answerContent = _v.answerContent.replace(/[;；]/,";\n");
+              _v.position = v.answers.length === 4 ? position4[_i] : position3[_i];
+              _v.bgTexture = "daan02";
+              _v.serial = {
+                value: serial[_i],
+                position: {
+                  x: 4,
+                  y: 74
+                }
+              }
+            });
+            return v;
+          })
+      }
+    }).then(() => {
+      this.loadAudio();
+    })
+  }
+
+  /**
+   * 加载音频
+   */
+  private loadAudio() {
+    // this.ccData.forEach(v => {
+    //   this._loader.audio(v.name, v.audioKey);
+    //   v.vocabularies.forEach(_v => {
+    //     this._loader.audio(_v.name, _v.audioKey);
+    //   })
+    // })
+    // this._loader.on("progress", (e: any) => {
+    //   e = Math.floor(50 + e * 50);
+    //   this.centerText.setText(`${e}%`);
+    // })
+    //this._loader.on("complete", () => {
+    this.scene.start('Game19PlayScene', {
+      data: this.ccData,
+      index: 0
+    });
+    //});
+    //this._loader.start();
+  }
+
+};
