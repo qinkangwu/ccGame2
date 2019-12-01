9c3f5e34294d91f59337212c9a844831f868f051
彭江 <jonny.peng@qq.com>
2019-12-01 09:32:04 +0800

    去掉css在外层适配屏幕的方法，只保留Phaser的FIT与CENTER.BOTH的方法

--- src/Public/jonny/core/resize.ts
+++ src/Public/jonny/core/resize.ts
@@ -8,14 +8,6 @@ export var resize = function (w,h){
     var content:HTMLElement = document.querySelector("#content");
     content.style.backgroundColor = "#000000";
     var canvas = document.querySelector("canvas");
-    this.scale.resize(w, h);
-    this.scale.scaleMode = Phaser.Scale.NONE;
-    canvas.setAttribute("style", `
-      width: 100% !important;
-      height: 100% !important;
-      object-fit: contain;
-      object-position: center;
-    `);
     deleteOther();
 }
 
